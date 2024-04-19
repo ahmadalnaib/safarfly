1976dede8e1fd0b7c5b5fa923adb29df0b25eaa6
ahmad.al-naib <alnaib888@gmail.com>
2024-04-19 15:10:14 +0200

    add tran

---
+++ resources/lang/ar/categories.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'museum' => 'ثقافة',
+    'tourist_attraction' => 'جولة في المدينة',
+    'park' => 'الطبيعية',
+    'amusement_park' =>'مغامرات',
+    'jewelry_store' => 'الفخامة',
+    'hospital' => 'الصحة',
+    'night_club' => 'الحياة الليلية',
+    'university' => 'التعليم',
+    'restaurant' => 'الطهي',
+    'church' => 'روحانية',
+    'spa' => 'الاسترخاء',
+    'park' => 'عائلية',
+    'shopping_mall' => 'التسوق',
+
+
+    // Add more categories here
+];
--- resources/views/includes/category_list.blade.php
+++ resources/views/includes/category_list.blade.php
@@ -3,7 +3,7 @@
     <input type="radio" id="category{{$category->id}}" name="categories" value="{{$category->id}}" class="hidden peer"  required>
     <label for="category{{$category->id}}" class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border-2 border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 peer-checked:border-red-600 hover:text-gray-600 dark:peer-checked:text-gray-300 peer-checked:text-gray-600 hover:bg-gray-50 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
       <div class="block">
-        <div class="w-full text-lg font-semibold">{{$category->title}}</div>
+        <div class="w-full text-lg font-semibold">{{ __('categories.' . $category->title) }}</div>
     </div>
     </label>
   </li>
